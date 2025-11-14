b16d6dcfe7a159e0ca0b4560e026e2455be7285b
hungdev <23130126@st.hcmuaf.edu.vn>
2025-11-14 19:43:50 +0700

    thêm phần đánh giá, comment, đổi position và thêm gợi í

--- src/js/Login.js
+++ src/js/Login.js
@@ -1,19 +1,22 @@
- document.getElementById("loginBtn").addEventListener("click", function() {
-    window.location.href = "homePage.html"; 
-  });
+
+//cái dưới là chuyển form đăng nhập đăng ký\
+document.getElementById("show-register").addEventListener("click", function (event) {
+  event.preventDefault();
+  document.querySelector(".container-signup").style.display = "none";
+  document.querySelector(".container-register").style.display = "block";
+});
 
 
+document.getElementById("show-login").addEventListener("click", function (event) {
+  event.preventDefault();
+  document.querySelector(".container-signup").style.display = "flex";
+  document.querySelector(".container-register").style.display = "none";
+});
 
-  //cái dưới là chuyển form đăng nhập đăng ký\
-  document.getElementById("show-register").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    document.querySelector(".container-signup").style.display = "none";
-    document.querySelector(".container-register").style.display = "block";
-  });
-  
-  
-  document.getElementById("show-login").addEventListener("click", function(event) {
-    event.preventDefault(); 
-    document.querySelector(".container-signup").style.display = "flex";
-    document.querySelector(".container-register").style.display = "none";
-  });
+
+//cái này làm cho xác thực quên mật khẩu
+document.getElementById("show-forgot-pw").addEventListener("click", function (event) {
+  event.preventDefault();
+  document.querySelector(".container-signup").style.display = "none";
+  document.querySelector(".forgot-pd-container").style.display = "block";
+});
