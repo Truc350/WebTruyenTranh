ad5e6abe94df9ddd4dde93298c35d8b34a819a36
Truc350 <23130350@st.hcmuaf.edu.vn>
2025-12-09 15:26:37 +0700

    push

---
+++ LTWeb_Nhom05/src/main/java/vn/edu/hcmuaf/fit/ltweb_nhom05/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.edu.hcmuaf.fit.ltweb_nhom05;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
