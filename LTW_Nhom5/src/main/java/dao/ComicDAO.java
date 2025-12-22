39440781c07697807c50b872d1f7db3952a75523
tram <23130341@st.hcmuaf.edu.vn>
2025-12-22 10:19:13 +0700

    -tạo controller, model - làm chưc năng tìm kiếm ở trang chủ

---
+++ LTW_Nhom5/src/main/java/dao/ComicDAO.java
@@ -0,0 +1,28 @@
+package dao;
+
+import model.Comic;
+
+import java.util.List;
+
+public class ComicDAO extends ADao {
+    public List<Comic> search(String keyword) {
+        String sql = """
+                    SELECT *
+                    FROM Comics
+                    WHERE is_deleted = 0
+                      AND status = 'available'
+                      AND (
+                            name_comics LIKE ?
+                         OR author LIKE ?
+                         OR publisher LIKE ?
+                      )
+                    ORDER BY created_at DESC
+                """;
+        return jdbi.withHandle(handle ->
+                handle.createQuery(sql)
+                        .bind("?", "%" + keyword + "%")
+                        .mapToBean(Comic.class)
+                        .list()
+        );
+    }
+}
--- LTW_Nhom5/src/main/java/db/JdbiConnector.java
+++ LTW_Nhom5/src/main/java/db/JdbiConnector.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Properties;
 
 public class JdbiConnector {
-//    private static final Logger log = LoggerFactory.getLogger(JdbiConnector.class);
+    //    private static final Logger log = LoggerFactory.getLogger(JdbiConnector.class);
     private static Jdbi jdbi;
 
     public static Jdbi get() {
@@ -79,10 +79,12 @@ public class JdbiConnector {
 //        System.out.println(product);
 //    }
     public static void main(String[] args) {
+//        System.out.println(">>> MAIN MỚI <<<");
         Jdbi jdbi = get();
-        jdbi.useHandle(handle -> {
-            handle.createQuery("select * from categories").mapTo(Category.class).forEach(System.out::println);
-        });
+//        jdbi.useHandle(handle -> {
+//            handle.createQuery("select * from categories").mapTo(Category.class).forEach(System.out::println);
+//        });
+        System.out.println("Kết nối DB OK: " + jdbi);
     }
 
 }
